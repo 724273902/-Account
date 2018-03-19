dccbe50e830233296b2673f56ba300a4e938158d
johnwall <724273902@qq.com>
2018-03-19 22:52:40 +0800

    Initial Commit

---
+++ 易教/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  易教
+//
+//  Created by john wall on 2018/3/19.
+//  Copyright © 2018年 john wall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
